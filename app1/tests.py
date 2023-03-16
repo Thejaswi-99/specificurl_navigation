7da477670538f33b0fe82931e75207adee646a22
Thejaswi-99 <thejaswijulupalli@gmail.com>
2023-03-16 19:40:00 +0530

    first commit

---
+++ app1/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+def dhoni(request):
+    return render(request,'dhoni.html'),
+
+# Create your tests here.
---
+++ app2/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+def virat(request):
+    return render(request,'virat.html'),
+
+# Create your tests here.
